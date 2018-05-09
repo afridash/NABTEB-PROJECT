9e00f9fd49beb90c645dea3182c847fd4fe1b00a
maybel66 <ogirikimabel@gmail.com>
2018-05-09 09:41:58 +0100

    Slider added with with tab for the home page done

--- FrontEnd/nabteb/src/components/Header.js
+++ FrontEnd/nabteb/src/components/Header.js
@@ -70,8 +70,8 @@ export default class Header extends Component {
                </div>
              </div>
          </div>
-         <nav className="navbar navbar-default" style={{backgroundColor:'#16a085'}}>
-     <div className="container-fluid" style={{marginLeft:'8%'}}>
+         <nav className="navbar navbar-default " style={{backgroundColor:'#16a085'}}>
+     <div className="container-fluid " style={{marginLeft:'8%'}}>
        <div className="navbar-header">
          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
            <span className="sr-only">Toggle navigation</span>
@@ -80,29 +80,33 @@ export default class Header extends Component {
            <span className="icon-bar"></span>
          </button>
        </div>
-       <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-         <ul className="nav navbar-nav">
-           <li onClick={()=>this.setState({selected:'home'})}> <Link  to='/' style={{color: this.state.selected === 'home' ? 'black' :'white'}}>HOME <span className="sr-only">(current)</span></Link></li>
-           <li onClick={()=>this.setState({selected:'funding'})}><Link to="/offers" style={{color:this.state.selected === 'funding' ? 'black' :'white'}}>OUR OFFER</Link></li>
-           <li onClick={()=>this.setState({selected:'training'})}><Link to="/examinations" style={{color:this.state.selected === 'training' ? 'black' :'white'}}>EXAMINATIONS</Link></li>
-           <li onClick={()=>this.setState({selected:'market'})}><Link to="/centers" style={{color:this.state.selected === 'market' ? 'black' :'white'}}>CENTERS</Link></li>
-           <li onClick={()=>this.setState({selected:'news'})}><Link to="/news" style={{color:this.state.selected === 'news' ? 'black' :'white'}}>NEWS</Link></li>
-           <li onClick={()=>this.setState({selected:'contact'})}><Link to="/contact" style={{color:this.state.selected === 'contact' ? 'black' :'white'}}>CONTACT US</Link></li>
-           <li onClick={()=>this.setState({selected:'register'})}>
-             {!this.state.loggedIn ? <Link style={{color:this.state.selected === 'register' ? 'black' :'white'}} to='/login'>REGISTER/LOGIN</Link>:
-               <Link onClick={()=>this.signOut()} style={{color:this.state.selected === 'contact' ? 'black' :'white'}}>
-               LOGOUT
-             </Link>
-           }
-           </li>
-         </ul>
-         {/* <div className='pull-right' style={{margin:5}}>
-           <Link  to='/cart'>
-           <img src={require('../images/cart.png')} style={{ height:40, width:40}} />
-           </Link>
-         </div> */}
+         <div className='col-sm-8 col-sm-offset-2'>
+           <div className="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
+             <ul className="nav navbar-nav">
+               <li onClick={()=>this.setState({selected:'home'})}> <Link  to='/' style={{color: this.state.selected === 'home' ? 'black' :'white'}}>HOME <span className="sr-only">(current)</span></Link></li>
+               <li onClick={()=>this.setState({selected:'funding'})}><Link to="/offers" style={{color:this.state.selected === 'funding' ? 'black' :'white'}}>OUR OFFER</Link></li>
+               <li onClick={()=>this.setState({selected:'training'})}><Link to="/examinations" style={{color:this.state.selected === 'training' ? 'black' :'white'}}>EXAMINATIONS</Link></li>
+               <li onClick={()=>this.setState({selected:'market'})}><Link to="/centers" style={{color:this.state.selected === 'market' ? 'black' :'white'}}>CENTERS</Link></li>
+               <li onClick={()=>this.setState({selected:'news'})}><Link to="/news" style={{color:this.state.selected === 'news' ? 'black' :'white'}}>NEWS</Link></li>
+               <li onClick={()=>this.setState({selected:'contact'})}><Link to="/contact" style={{color:this.state.selected === 'contact' ? 'black' :'white'}}>CONTACT US</Link></li>
+               <li onClick={()=>this.setState({selected:'register'})}>
+                 {!this.state.loggedIn ? <Link style={{color:this.state.selected === 'register' ? 'black' :'white'}} to='/login'>REGISTER/LOGIN</Link>:
+                   <Link onClick={()=>this.signOut()} style={{color:this.state.selected === 'contact' ? 'black' :'white'}}>
+                   LOGOUT
+                 </Link>
+               }
+               </li>
+             </ul>
+             {/* <div className='pull-right' style={{margin:5}}>
+               <Link  to='/cart'>
+               <img src={require('../images/cart.png')} style={{ height:40, width:40}} />
+               </Link>
+             </div> */}
 
-       </div>
+           </div>
+         </div>
+
+
      </div>
    </nav>
    <div className='row hidden-lg hidden-sm hidden-md' style={{margin:10}}>

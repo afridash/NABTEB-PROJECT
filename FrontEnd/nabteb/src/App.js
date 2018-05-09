579434c4ed05bdf2800f560479285d1952a23c1c
Richard Igbiriki <imorobebh@gmail.com>
2018-05-09 14:03:45 +0100

    merge with home page

--- FrontEnd/nabteb/src/App.js
+++ FrontEnd/nabteb/src/App.js
@@ -52,23 +52,23 @@ class App extends Component {
         </ul>
         <div className='row' style={{marginTop:15, padding:10, }}>
         <div className='col-sm-4' style={{borderRadius:5, fontSize:20}}>
-          <h3 style={{color:'#2980b9'}}>Mission</h3>
+          <h3 className='text-center' style={{color:'#2980b9'}}>Mission</h3>
           <div style={{borderBottomStyle:'solid', borderWidth:'1px', margin:10, textAlign:'justify'}}>
             <h4 style={{lineHeight:1.3, fontFamily:"Times New Roman, Times, serif"}}>Become a Globally Acknowledged Assessment Body for Craftsmen and Technicians.</h4>
           </div>
-          <h3 style={{color:'#2980b9'}}>Vision</h3>
+          <h3 className='text-center' style={{color:'#2980b9'}}>Vision</h3>
           <div style={{borderBottomStyle:'solid', borderWidth:'1px', textAlign:'justify', marginLeft:10}}>
             <h4 style={{lineHeight:1.3, fontFamily:"Times New Roman, Times, serif"}}>NABTEB is a vision-led, mission-driven public institution with a vision:
               Be a Globally Recognized Assessment and Certification Body Preparing Candidates for the World of Work and Academic/Professional Excellence.</h4>
           </div>
-          <h3 style={{color:'#2980b9'}}>Brief of NABTEB</h3>
+          <h3 className='text-center' style={{color:'#2980b9'}}>Brief of NABTEB</h3>
           <div style={{textAlign:'justify', marginLeft:10}}>
             <h4 style={{lineHeight:1.3, fontFamily:"Times New Roman, Times, serif"}}>The National Business and Technical Examinations Board was established in 1992 to domesticate craft level examinations which were hitherto conducted by City & Guilds, Pittman’s, and Royal Society of Arts all of UK in accordance with the provisions of the National Policy on Education. Its establishment was the climax of an evolutionary process which spanned 15 years from 1977 to 1992 and in which FOUR Government Panels at different times were set up to review the place and structure of public examinations in our educational system.</h4>
           </div>
 
         </div>
         <div className='col-sm-4' style={{fontSize:20}}>
-          <h3 style={{color:'#2980b9'}}>Quick Links</h3>
+          <h3 className='text-center' style={{color:'#2980b9'}}>Quick Links</h3>
           <div style={{border:'1px solid black', borderRadius:4, textAlign:'left', fontFamily:"Times New Roman, Times, serif"}}>
             <ul>
               <li><h4><Link to='#'>Entry Guide</Link></h4></li>
@@ -86,7 +86,7 @@ class App extends Component {
 
         </div>
         <div className='col-sm-4' style={{fontSize:20, marginTop:-5}}>
-          <h3 style={{color:'#2980b9'}}>News & Events</h3>
+          <h3 className='text-center' style={{color:'#2980b9'}}>News & Events</h3>
           <div style={{marginTop:-10}}>
             <div className='row' style={{borderBottomStyle:'solid', borderWidth:'1px', margin:5}}>
               <div className='col-sm-4'>
